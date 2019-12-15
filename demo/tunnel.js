20812458e513fd4e1bcc83d5c1698fb54f949f51
Simon Mollweide <simon.mollweide@namics.com>
2019-12-15 19:41:43 +0100

    fix: adjust bodyParser limit to 50mb #133

--- demo/tunnel.js
+++ demo/tunnel.js
@@ -25,7 +25,7 @@ var logFunc = function () {
 	}
 };
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
